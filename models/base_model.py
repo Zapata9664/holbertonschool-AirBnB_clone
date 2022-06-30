e42bd58bc1db894d4c8282518dbfe4da690599f5
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-30 17:10:29 -0500

    test 3

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module whit class BaseModel"""
+
+from pydoc import classname
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """class that defines all common
+    attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Inizialite BaseModel"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Dictionary from save class"""
+        BaseDict = self.__dict__.copy()
+        BaseDict["__class__"] = self.__class__.__name__
+        BaseDict["created_at"] = self.created_at.isoformat()
+        BaseDict["updated_at"] = self.updated_at.isoformat()
+        return (BaseDict)
+
+    def __str__(self):
+        """Containing all keys/values of __dict__ of the instance"""
+        classname = self.__class__.__name__
+        return (f"[{classname}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """Updates the public instance attribute
+        updated_atwith the current datetime"""
+        self.updated_at = datetime.now()
