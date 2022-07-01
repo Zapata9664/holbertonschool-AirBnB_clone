68b2fad5172ef2e1e4cbd08653bb5167b08f7770
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-07-01 13:16:16 -0500

    task 5

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""Module for Class User from BaseModel"""
 from models.base_model import BaseModel
 
 class User(BaseModel):
-    """"""
+    """"Class User that inherits from BaseMode"""
     email = ""
     password = ""
     first_name = ""
