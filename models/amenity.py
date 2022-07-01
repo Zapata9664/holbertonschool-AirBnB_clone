c701f0b92367ce5bda7f762abe0a6543bfddf581
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-07-01 13:53:06 -0500

    task 6

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for Amenity from BaseModel"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Class Amenity that inherits from BaseMode"""
+    name = ""
+    
