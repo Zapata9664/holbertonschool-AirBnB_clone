73f5ff4b1a0d8c368f9775eae38b53dac17780e9
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-07-01 17:09:47 -0500

    make console

--- console.py
+++ console.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """Module with class HBNBComman"""
-import cmd
+import cmd, sys
+from pickle import TRUE
 
 
 class HBNBCommand(cmd.Cmd):
     """program that contains the entry
     point of the command interpreter"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        sys.exit()
+
+    def do_EOF(self, arg):
+        """Exit the program whit ctrl + D"""
+        return True
+    
+    def do_help(self, arg):
+        """Print commands arguments"""
+        cmd.Cmd.do_help(self, arg)
+
+    def emptyline(self):
+        """An empty line + ENTER shouldn’t execute anything"""
+        print ("llegue")
+        pass
+
+    
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
