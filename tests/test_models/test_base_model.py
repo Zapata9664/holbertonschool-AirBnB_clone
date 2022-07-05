b961b3ca1b6eebbd17cc0c55a252c809c444c14c
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-07-05 11:31:11 -0500

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,13 +3,22 @@
 
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
+from time import sleep
 
 
 class testBaseModel(unittest.TestCase):
     """Test for BaseModel Class"""
-    def test_same_string(self):
+
+    def test_equal_string(self):
         """Check if id is a string"""
-        self.assertNotEqual(str, BaseModel().id)
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_unique_id(self):
+        """Check if the id in unique"""
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        self.assertNotEqual(instance1.id, instance2.id)
 
     def test_instance_id(self):
         """Check if id is a intance of BaseModel"""
@@ -22,6 +31,28 @@ class testBaseModel(unittest.TestCase):
     def test_instance(self):
         """Check if updated_at is a intance of BaseModel"""
         self.assertTrue(hasattr(BaseModel(), "updated_at"), True)
+
+    def test_public_datetime(self):
+        """Test for a make public datetime"""
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_public_datetime_updated(self):
+        """Test for a make public datetime"""
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_less_time_createdat(self):
+        """Check less time in created_at"""
+        instance1 = BaseModel()
+        sleep(0.2)
+        instance2 = BaseModel()
+        self.assertLess(instance1.created_at, instance2.created_at)
+
+    def test_less_time_updatedat(self):
+        """Check less time in updated_at"""
+        instance1 = BaseModel()
+        sleep(0.2)
+        instance2 = BaseModel()
+        self.assertLess(instance1.updated_at, instance2.updated_at)
 
 
 if __name__ == '__main__':
