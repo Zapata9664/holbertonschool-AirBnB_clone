f23c0c38f80934ff45448ea8fc76646ff69d8c9d
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-07-05 10:22:27 -0500

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,3 +10,19 @@ class testBaseModel(unittest.TestCase):
     def test_same_string(self):
         """Check if id is a string"""
         self.assertNotEqual(str, BaseModel().id)
+
+    def test_instance_id(self):
+        """Check if id is a intance of BaseModel"""
+        self.assertTrue(hasattr(BaseModel(), "id"), True)
+
+    def test_instance_createdat(self):
+        """Check if created_at is a intance of BaseModel"""
+        self.assertTrue(hasattr(BaseModel(), "created_at"), True)
+
+    def test_instance(self):
+        """Check if updated_at is a intance of BaseModel"""
+        self.assertTrue(hasattr(BaseModel(), "updated_at"), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
